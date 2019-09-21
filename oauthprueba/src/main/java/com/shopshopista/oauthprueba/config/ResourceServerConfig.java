97d42ebda8d6b87f3f5740a75373fe4ca977c5e2
Johnny <johnnygar98@hotmail.com>
2019-09-21 18:44:31 -0500

    Sigue sin funcionar esta cosa loca

--- oauthprueba/src/main/java/com/shopshopista/oauthprueba/config/ResourceServerConfig.java
+++ oauthprueba/src/main/java/com/shopshopista/oauthprueba/config/ResourceServerConfig.java
@@ -3,34 +3,34 @@ package com.shopshopista.oauthprueba.config;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfiguration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 /**
  *
  * @author gus
  */
 @EnableResourceServer
-public class ResourceServerConfig extends ResourceServerConfiguration {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(HttpSecurity http) throws Exception {
         // Configurando para el token y que puedan probar peticiones sin authentificacion
         http.authorizeRequests()
                 .antMatchers(
                         "/oauth/token",
-                        "/oauth/authorize**",
-                        "/api/"
+                        "/oauth/authorize**"
                 ).permitAll();
         // Configurando las demas peticiones 
         http.requestMatchers()
-                .antMatchers("/saluda")
+                .antMatchers("/api/saluda")
                 .and().authorizeRequests()
-                .antMatchers("/saluda").access("hasRole(USER)")
+                .antMatchers("/api/saluda").access("hasRole(USER)")
                 .and()
                 .requestMatchers()
-                .antMatchers("/mensaje")
+                .antMatchers("/api/mensaje")
                 .and()
                 .authorizeRequests()
-                .antMatchers("/mensaje")
+                .antMatchers("/api/mensaje", "/api/personas")
                 .access("hasRole(ADMIN)");
     }
     
